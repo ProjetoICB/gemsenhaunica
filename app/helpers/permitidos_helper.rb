581e6cce662541f5d7a6e6ce3641365ec783042c
Fabiana Munhoz <biamunhoz@gmail.com>
2018-10-08 10:14:54 -0300

    Completo

---
+++ app/helpers/permitidos_helper.rb
@@ -0,0 +1,2 @@
+module PermitidosHelper
+end
---
+++ db/schema.rb
@@ -0,0 +1,76 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20181001184044) do
+
+  create_table "logs", force: :cascade do |t|
+    t.string   "acao",       limit: 255
+    t.string   "ip",         limit: 255
+    t.integer  "usuario_id", limit: 4
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  add_index "logs", ["usuario_id"], name: "index_logs_on_usuario_id", using: :btree
+
+  create_table "perfils", force: :cascade do |t|
+    t.string   "tipo",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  create_table "permitidos", force: :cascade do |t|
+    t.integer  "usuario_id", limit: 4
+    t.integer  "perfil_id",  limit: 4
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+  end
+
+  add_index "permitidos", ["perfil_id"], name: "index_permitidos_on_perfil_id", using: :btree
+  add_index "permitidos", ["usuario_id"], name: "index_permitidos_on_usuario_id", using: :btree
+
+  create_table "tipo_vinculos", force: :cascade do |t|
+    t.string   "tipoVinculo",         limit: 255
+    t.string   "codigoSetor",         limit: 255
+    t.string   "nomeAbreviadSetor",   limit: 255
+    t.string   "nomeSetor",           limit: 255
+    t.string   "codigoUnidade",       limit: 255
+    t.string   "siglaUnidade",        limit: 255
+    t.string   "nomeUnidade",         limit: 255
+    t.string   "nomeVinculo",         limit: 255
+    t.string   "nomeAbreviadoFuncao", limit: 255
+    t.integer  "usuario_id",          limit: 4
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+  end
+
+  add_index "tipo_vinculos", ["usuario_id"], name: "index_tipo_vinculos_on_usuario_id", using: :btree
+
+  create_table "usuarios", force: :cascade do |t|
+    t.string   "nomeUsuario",             limit: 255
+    t.string   "loginUsuario",            limit: 255
+    t.string   "tipoUsuario",             limit: 255
+    t.string   "emailPrincipalUsuario",   limit: 255
+    t.string   "emailAlternativoUsuario", limit: 255
+    t.string   "emailUspUsuario",         limit: 255
+    t.string   "numeroTelefoneFormatado", limit: 255
+    t.string   "ramalUsp",                limit: 255
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+  end
+
+  add_foreign_key "logs", "usuarios"
+  add_foreign_key "permitidos", "perfils"
+  add_foreign_key "permitidos", "usuarios"
+  add_foreign_key "tipo_vinculos", "usuarios"
+end
